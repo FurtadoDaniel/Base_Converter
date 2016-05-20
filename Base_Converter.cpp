33d1c661cf93004cd371ad79d025c09d2bd835b8
João Melo <joao_melo@id.uff.br>
2016-05-20 12:55:15 -0300

    NC2
    Fiz algumas funções, mas não testei nenhuma ainda. Todas foram feitas atendendo, no máximo, a base 16.

--- Base_Converter.cpp
+++ Base_Converter.cpp
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> //nao sei se essa biblioteca eh necessaria mesmo, da um confere aí
@@ -6,7 +8,7 @@ int verify (int k, char *n); //recebe a base original e o endereço da primeira 
 int toInt (char n); //recebe a primeira posicao do numero
 int adaptHex (char n); //trabalha junto com toInt() para adatar os valores dos numeros hexadecimais, recebendo o valor q deve converter
 int kToTen (int k, int *n); //recebe a base original do numero e a primeira posicao do do vetor gerado com toInt()
-int tenToK (int n, int k); //recebe o numero inteiro gerado com kToTen() e a base para a qual converter o numero
+void tenToK (int n, int k); //recebe o numero inteiro gerado com kToTen() e a base para a qual converter o numero
 
 int main(){
 	int b, k, num[], base_ten;
@@ -23,7 +25,7 @@ int main(){
 			num[i] = toInt (n[i]);
 			i++;
 		}
-		base_ten = kToTen (k, num);
+		base_ten = kToTen (k, num, strlen (n));
 	}
 	else {
 		//faça aqui um código q pegue o numero char n, que vai estar na base int b == 10, para um int base_ten de mesmo valor
@@ -31,9 +33,50 @@ int main(){
 	if (k == 10)
 		printf ("\nResultado %d", base_ten);
 	else {
-		int result[]; //ou char result[];
-		tenToK (base_ten, result);
+		printf ("Resultado: ");
+		tenToK (int n, int k);
 		//Insira um codigo para exibir o resultado
 		//O resultado em hexadecial pode ser exibido usando %x, mas eu não sei se vai funcionar, ou transformando o resultado em char novamente.
 	}
+	return 0;
+}
+
+int kToTen (int k, int *n, int tam){
+	int num = 0, i;
+	for (i = 0; i < tam; i++, n++)
+		num += *n * pow (k, i);
+	return num;
+}
+
+void tenToK (int n, int k){
+	tenToK (n / k);
+	if (k == 10){
+		switch (n % k){
+			case 10: printf ("a"); break;
+			case 11: printf ("b"); break;
+			case 12: printf ("c"); break;
+			case 13: printf ("d"); break;
+			case 14: printf ("e"); break;
+			case 15: printf ("f"); break;
+		}
+	}
+	else
+		printf ("%d", n % k);
+}
+
+int verify (int k, char *n){
+	char asc;
+	switch (k){
+		case 2: asc = '1'; break;
+		case 5: asc = '4'; break;
+		case 8: asc = '7'; break;
+		case 10: asc = '9'; break;
+		case 16: asc = 'f'; break;
+	}
+	while (*n != '\0'){
+		if (tolower (*n) > asc)
+			return 1;
+		*n++;
+	}
+	return 0;
 }
